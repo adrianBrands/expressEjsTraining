116747fc3c587659a130996dc081a82c7c7b8e8f
Adrian Brandshaug <adrianbrandshaug@hotmail.com>
2025-02-09 15:07:10 +0100

    installed tailwind and tested styling

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("index", { text: "This small project using Tailwind with Express and Ejs", title: "Express, Ejs, Tailwind" });
 });
 
 module.exports = router;
